3c6c34b45ab9d989d78b4a6452a80823cd10e443
Kirill Romanov <kirban95@gmail.com>
2022-06-23 13:39:34 +0300

    feat: create connection between docker images for app and mysql db

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:14
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+# For production use:
+# RUN npm ci --only=production
+COPY . .
+EXPOSE 8000
+EXPOSE 9229
+CMD npm start
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
 const {
-  PORT = 8000,
-  DB_HOST: host,
+  API_PORT = 8080,
+  DB_HOST: host = 'localhost',
   DB_USER: user,
   DB_PASS: password,
   DB_NAME: database,
+  DB_PORT: dbPort = 3306,
 } = process.env;
 const express = require('express');
 const mysql = require('mysql');
@@ -16,18 +17,19 @@ const dbConnection = mysql.createConnection({
   user,
   password,
   database,
+  port: dbPort,
 });
 dbConnection.connect();
 const server = express();
 
 server.use(authRouter);
 
-server.listen(PORT, () => {
+server.listen(API_PORT, () => {
   dbConnection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
   });
 
   dbConnection.end();
-  console.info(`Server is listening on port ${PORT}`);
+  console.info(`Server is listening on port ${API_PORT}`);
 });
