9f77c0b53c70a06a2dfa635a050c17597bf3d960
Kirill Romanov <kirban95@gmail.com>
2022-07-05 12:43:58 +0300

    fix: initial data structures for db

--- .db-init/setup.sql
+++ .db-init/setup.sql
@@ -5,13 +5,14 @@ USE `my_db`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `hash` VARCHAR(255),
+  `userName` VARCHAR(20),
   `firstName` VARCHAR(20),
   `lastName`VARCHAR(20),
   `age` INT(10),
   `gender` VARCHAR(10),
   `city` VARCHAR(10),
   `interests` VARCHAR(255),
-  `page` INT,
+  `pageId` INT,
   PRIMARY KEY `pk_id`(`id`)
 ) ENGINE = InnoDB;
 
