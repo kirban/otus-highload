f3b3945e696d071320b7e64c46f7d182e59ff071
Kirill Romanov <kirban95@gmail.com>
2022-07-14 14:12:07 +0300

    feat: join user on find all request for pages

--- server/models/page.js
+++ server/models/page.js
@@ -4,6 +4,23 @@ class Page extends Model {
   constructor() {
     super('personal_pages');
   }
+
+  async findAll() {
+    return this.pool.query(`SELECT * FROM ${this.tableName} INNER JOIN users ON users.pageId=personal_pages.id`)
+      .then((result) => this.mapReturnedResult(result))
+      .catch((e) => { throw new Error(`Failed to fetch all ${this.tableName}:\n${e.message}`); });
+  }
+
+  mapReturnedResult([rows, _columns]) {
+    const formattedRows = rows.map((row) => {
+      const {
+        id, title, hash, pageId, ...user
+      } = row;
+      return { id, title, user };
+    });
+
+    return formattedRows.insertId ? formattedRows.insertId : formattedRows;
+  }
 }
 
 const page = new Page();
