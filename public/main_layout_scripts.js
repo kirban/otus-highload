26fc12b90c3742ab29d14cb414353c7b89de00a1
Kirill Romanov <kirban95@gmail.com>
2022-07-25 19:46:10 +0300

    feat: implement searching user pages on front side

--- public/main_layout_scripts.js
+++ public/main_layout_scripts.js
@@ -14,3 +14,40 @@ document.getElementById('logout').onclick = (e) => {
   removeAllCookies();
   window.location.replace('/login');
 };
+
+document.getElementById('searchInput').oninput = (e) => {
+  function addItemToList(itemText) {
+    const listItem = document.createElement('a');
+    listItem.className = 'dropdown-item';
+    listItem.innerText = itemText;
+    document.getElementById('searchDropdown').appendChild(listItem);
+  }
+  function clearResults() {
+    const dropdown = document.getElementById('searchDropdown');
+
+    if (dropdown.childElementCount > 1) {
+      Array.prototype.slice.call(dropdown.children).forEach((child, idx) => {
+        if (idx !== 0) {
+          dropdown.removeChild(child);
+        }
+      });
+    }
+  }
+  clearResults();
+  $('#searchInput').dropdown('hide');
+  const trimmedInput = e.target.value.trim().replace(/ /g, '');
+  if (trimmedInput.length > 2) {
+    $('#searchInput').dropdown('show');
+    fetch(`/api/pages/search?text=${trimmedInput}`)
+      .then((r) => r.json())
+      .then((pages) => {
+        if (pages.length === 0) {
+          addItemToList('Не удалось никого найти :(');
+        }
+        pages.forEach(({ user }) => {
+          addItemToList(`${user.firstName} ${user.lastName} - ${user.userName}`);
+        });
+      })
+      .catch(console.error);
+  }
+};
